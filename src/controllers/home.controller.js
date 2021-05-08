272060da41f7813df9bdcea0fdf5031112ca8c6b
Lunadiotic <ahmadhakim404@gmail.com>
2021-05-08 20:07:26 +0700

    make homepage and bind data

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,3 +1,14 @@
 exports.home = (req, res) => {
-    res.render('index')
+    let data = [{
+        title: 'Man must explore, and this is exploration at its greatest',
+        subtitle: 'Problems look mighty small from 150 miles up',
+        date: 'on September 24, 2021'
+    }, {
+        title: 'I believe every human has a finite number of heartbeats. I don\'t intend to waste any of mine.',
+        subtitle: '',
+        date: 'on September 24, 2021'
+    }]
+    res.render('index', {
+        data
+    })
 }
