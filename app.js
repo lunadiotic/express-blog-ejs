a5544c86ee8dd13bb4f25b697a6040f4ffa5f678
Lunadiotic <ahmadhakim404@gmail.com>
2021-05-08 11:16:33 +0700

    make express ejs view engine project boilerplate

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.send('hello, world')
 })
+
+require('./src/routes/home.routes')(app)
 
 const PORT = 5000
 app.listen(PORT, () => {
